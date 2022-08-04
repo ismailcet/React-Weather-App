be1c7d2ff196f30ddc821a9bec85007f6e58a61d
ismailcet <ismailcet3@gmail.com>
2022-08-04 17:54:24 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const url =
+    "https://wft-geo-db.p.rapidapi.com/v1/geo/cities?countryIds=TR&namePrefix=hatay";
+
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "6327be9192msh3c60375df64e7ddp1b4b20jsn1dfdf5dbaf50",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((json) => console.log(json))
+    .catch((err) => console.error("error:" + err));
+  return <div className="App"></div>;
 }
 
 export default App;
